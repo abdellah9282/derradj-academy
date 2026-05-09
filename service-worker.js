537e10676db29cfb9c3b0349834bb722a7311ec8
abdellahderradj <216191300+abdellah9282@users.noreply.github.com>
2026-05-09 15:17:16 +0100

    summary

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,16 @@
-self.addEventListener('install', (e) => {
+self.addEventListener("install", (e) => {
+  console.log("Service Worker Installed");
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
+self.addEventListener("activate", (e) => {
+  console.log("Service Worker Activated");
   e.waitUntil(clients.claim());
 });
 
 // Required for Chrome to consider the site PWA-installable
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
